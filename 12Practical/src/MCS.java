12717cb663d5d0c193bc3fd2ace95a3ad85c07fc
ItsAJ_G <nathigumede339@gmail.com>
2026-02-16 14:41:57 +0200

    initialising the counters

---
+++ 12Practical/src/MCS.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class MCS {
+    //Global Counters
+    static long countOn3=0;
+    static long countOn2=0;
+    static long countOn1=0;
+    static long countOn0=0;
+
+
+}
