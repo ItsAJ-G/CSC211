7c6c660ff368c2f39e03728b2e8602e1abec1f6b
ItsAJ_G <nathigumede339@gmail.com>
2026-02-16 14:55:34 +0200

    O(N^3)

--- 12Practical/src/MCS.java
+++ 12Practical/src/MCS.java
@@ -7,5 +7,25 @@ public class MCS {
     static long countOn1=0;
     static long countOn0=0;
 
+    //Implementation of O(n^3)
+    public static int mcsOn3(int[] X){
+        int n=X.length;
+        int Max=0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int total=0;
+                for (int k = 0; k < n; k++) {
+                    total+=X[k];
+                    countOn3++;
+                }
+                if (total>Max){
+                    Max=total;
+                }
+            }
+        }
+        return max;
+    }
+
 
 }
