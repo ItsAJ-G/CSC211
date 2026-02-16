cf512ade9ef10447080452b05acd3e3b7ce48fb9
ItsAJ_G <nathigumede339@gmail.com>
2026-02-16 15:17:41 +0200

    O(N^2) -->B It was a long one

--- 12Practical/src/MCS.java
+++ 12Practical/src/MCS.java
@@ -3,8 +3,8 @@ import java.util.Random;
 public class MCS {
     //Global Counters
     static long countOn3=0;
-    static long countOn2=0;
-    static long countOn1=0;
+    static long countOn2A=0;
+    static long countOn2B=0;
     static long countOn0=0;
 
     //Implementation of O(n^3)
@@ -28,8 +28,8 @@ public class MCS {
     }
 
 
-    //Implementing o(n^2)
-    public static int mcsOn2(int[] X){
+    //Implementing o(n^2) A
+    public static int mcsOn2A(int[] X){
         int n=X.length;
         int Max=0;
 
@@ -37,7 +37,31 @@ public class MCS {
             int total=0;
             for (int k = 0; k < n; k++) {
                 total+=X[k];
-                countOn2++;
+                countOn2A++;
+                if (total>Max){
+                    Max=total;
+                }
+            }
+        }
+        return Max;
+    }
+
+    //Implementing O(n^2)B
+
+    public static int mcsOn2B(int[] X){
+        int n=X.length;
+        int[] totalTo=new int[n+1];
+
+        for (int r = 0; r < n; r++) {
+            totalTo[r]=totalTo[r-1]+X[r-1];
+        }
+
+        int Max=0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j <n; j++) {
+                int total=totalTo[j+1]-totalTo[i];
+                countOn2B++;
                 if (total>Max){
                     Max=total;
                 }
