4141e4f01f7c188f1b781f1180660626849aaa2d
ItsAJ_G <nathigumede339@gmail.com>
2026-03-02 14:42:30 +0200

    finishing up the open hash

--- 14Practical/src/OpenHash.java
+++ 14Practical/src/OpenHash.java
@@ -15,5 +15,47 @@ public class OpenHash {
         return (h % m) + 1;
     }
 
+    public boolean isFull() {
+        return size==m;
+    }
 
+    public boolean isEmpty() {
+        return size==0;
+    }
+
+    public boolean isInTable(String key) {
+        return lookup(key) != null;
+    }
+
+    public void insert(String key, String value) {
+        if(isFull()){
+            return;
+        }
+        int i = hash(key);
+        int start = i;
+
+        while (table[i].key.equals(key)) {
+            table[i].value=value;
+            return;
+        }
+
+        i=(i%m)+1;
+        if(i==start){
+            return;
+        }
+
+        table[i]=new KeyValue(key,value);
+        size++;
+    }
+
+    public String lookup(String key) {
+        int i = hash(key);
+        int start = i;
+        while (table[i]!=null) {
+            if (table[i].key.equals(key)) {
+                return table[i].value;
+            }
+
+        }
+    }
 }
