19eb9dcd5ef988fc9f3cc188015f4892ea2670a3
ItsAJ_G <nathigumede339@gmail.com>
2026-03-02 14:27:58 +0200

    have a lot more to code

--- 14Practical/src/OpenHash.java
+++ 14Practical/src/OpenHash.java
@@ -1,3 +1,19 @@
 public class OpenHash {
+    private KeyValue[] table ;
+    private int m;
+    private int size;
+
+    public OpenHash(int size) {
+        this.m=m;
+        table = new KeyValue[m+1];
+        size=0;
+    }
+
+    public int hash(String key) {
+        int h = key.hashCode();
+        if (h < 0) h = -h;
+        return (h % m) + 1;
+    }
+
 
 }
