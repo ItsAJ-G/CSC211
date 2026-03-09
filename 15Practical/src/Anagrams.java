2b4138000853879036e43b733201e301e2db37d3
ItsAJ_G <nathigumede339@gmail.com>
2026-03-09 14:37:37 +0200

    strip strings and special characters

--- 15Practical/src/Anagrams.java
+++ 15Practical/src/Anagrams.java
@@ -4,10 +4,23 @@ import java.io.*;
 
 public class Anagrams {
 
-    static  String signature(String word){
+    static  String signature(String word) {
         char[] arr = word.toCharArray();
         Arrays.sort(arr);
         return new String(arr);
+    }
+
+    static String stripPunctuation(String word) {
+        String punctuation="[]0123456789(,.:;_.-?!)";
+        int start=0;
+        int end=word.length();
+        while(start<end && punctuation.indexOf(word.charAt(start))>=0) {
+            start++;
+        }
+        while (end>start && punctuation.indexOf(word.charAt(end-1))>=0) {
+            end--;
+        }
+        return word.substring(start,end);
     }
 
 
