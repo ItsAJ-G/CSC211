0bcb4e493de8215cf6b68a1b91fc2257f8cb9bb8
ItsAJ_G <nathigumede339@gmail.com>
2026-03-09 15:17:55 +0200

    group words

--- 15Practical/src/Anagrams.java
+++ 15Practical/src/Anagrams.java
@@ -5,23 +5,23 @@ import java.io.*;
 
 public class Anagrams {
 
-    static  String signature(String word) {
+    static String signature(String word) {
         char[] arr = word.toCharArray();
         Arrays.sort(arr);
         return new String(arr);
     }
 
     static String stripPunctuation(String word) {
-        String punctuation="[]0123456789(,.:;_.-?!)";
-        int start=0;
-        int end=word.length();
-        while(start<end && punctuation.indexOf(word.charAt(start))>=0) {
+        String punctuation = "[]0123456789(,.:;_.-?!)";
+        int start = 0;
+        int end = word.length();
+        while (start < end && punctuation.indexOf(word.charAt(start)) >= 0) {
             start++;
         }
-        while (end>start && punctuation.indexOf(word.charAt(end-1))>=0) {
+        while (end > start && punctuation.indexOf(word.charAt(end - 1)) >= 0) {
             end--;
         }
-        return word.substring(start,end);
+        return word.substring(start, end);
     }
 
     public static void main(String[] args) throws IOException {
@@ -32,22 +32,27 @@ public class Anagrams {
         String inputFile = args[0];
         System.out.println("Input file: " + inputFile);
 
-        Map<String,Integer> wordCount=new LinkedHashMap<>();
+        Map<String, Integer> wordCount = new LinkedHashMap<>();
         try (BufferedReader br = new BufferedReader(
                 new InputStreamReader(new FileInputStream("joyce1922_ulysses.text"),
                         Charset.forName("ISO-8859-1")))) {
             String line;
             while ((line = br.readLine()) != null) {
-                String[] tokens=line.split("\\s+");
+                String[] tokens = line.split("\\s+");
                 for (String token : tokens) {
-                    String w=stripPunctuation(token);
-                    if(!w.isEmpty()) {
+                    String w = stripPunctuation(token);
+                    if (!w.isEmpty()) {
                         wordCount.merge(w, 1, Integer::sum);
                     }
                 }
             }
         }
+        Map<String, List<String>> anagrams = new LinkedHashMap<>();
+        for (String word : wordCount.keySet()) {
+            String sig= signature(word);
+            anagramGroups.computeIfAbsent(sig,k->new ArrayList<>().add(word));
+        }
+
+
     }
-
-
 }
