d183a973f830a4e825a703798efb1d8c099b7966
ItsAJ_G <nathigumede339@gmail.com>
2026-03-16 14:34:41 +0200

    Bottom up heap build top up heap build (repeated insert)

--- 16Prac/src/tryHeapsort.java
+++ 16Prac/src/tryHeapsort.java
@@ -33,6 +33,32 @@ public class tryHeapsort {
         }
     }
 
+    //(a)Bottom-up Heap build
 
+    static void buildHeapBottomUp(String[] arr) {
+        int n = arr.length;
 
+        for (int i=n/2-1;i>=0;i--) {
+            shiftdown(arr, i, n);
+        }
+    }
+
+    //(b)Top-Down Heap build
+
+    static void shiftup(String[] arr, int i) {
+        while (i>0 && arr[parent(i)].compareTo(arr[i])>0) {
+            swap(arr, i, parent(i));
+            i=parent(i);
+        }
+    }
+
+    static void insertTopDown(String[] arr, int i) {
+        shiftup(arr, i);
+    }
+
+    static void buildHeapTopDown(String[] arr) {
+        for (int i=1;i<arr.length;i++) {
+            insertTopDown(arr, i);
+        }
+    }
 }
