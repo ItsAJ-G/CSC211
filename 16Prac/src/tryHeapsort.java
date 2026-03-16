c389eb7b6e8df306012b852d9333d3e3e7dd589c
ItsAJ_G <nathigumede339@gmail.com>
2026-03-16 14:19:43 +0200

    Done with the heap helpers

--- 16Prac/src/tryHeapsort.java
+++ 16Prac/src/tryHeapsort.java
@@ -6,4 +6,15 @@ public class tryHeapsort {
     static String[] heap;
     static int heapSize;
 
+    //Heap Helpers
+    static int parent(int i) {return (i - 1) / 2;}
+    static int left(int i) {return 2*i+1;}
+    static int right(int i) {return 2*i+2;}
+
+    static void swap(String[] arr, int i, int j) {
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 }
