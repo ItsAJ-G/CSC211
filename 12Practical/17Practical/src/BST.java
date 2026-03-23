a3237b48d7e59171a08f820e6ac7e9544f8e4b5e
ItsAJ_G <nathigumede339@gmail.com>
2026-03-23 15:06:20 +0200

    Constructors with and without argument

--- 12Practical/17Practical/src/BST.java
+++ 12Practical/17Practical/src/BST.java
@@ -1,6 +1,32 @@
 //They were on my laptop for sometime cause i had issues
 //Ended up restarting project
 
-public class BST {
+//Nathi Gumede
+//4533277
 
+import java.util.*;
+
+public class BST {
+    class tNode {
+        int key;
+        tNode left;
+        tNode right;
+
+        //Constructor with arguement
+
+        public tNode(int key) {
+            this.key = key;
+            this.left = null;
+            this.right = null;
+        }
+
+        //Constructor without arguement
+        public tNode() {
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+    //BST CLass
 }
