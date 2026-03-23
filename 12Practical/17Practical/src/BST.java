46143dbb0d1bf7bff35d6c503844ad6fd3c6f7fc
ItsAJ_G <nathigumede339@gmail.com>
2026-03-23 15:15:40 +0200

    BST CLASS Insert method

--- 12Practical/17Practical/src/BST.java
+++ 12Practical/17Practical/src/BST.java
@@ -7,7 +7,7 @@
 import java.util.*;
 
 public class BST {
-    class tNode {
+    static class tNode {
         int key;
         tNode left;
         tNode right;
@@ -28,5 +28,27 @@ public class BST {
 
     }
 
-    //BST CLass
+    //BST CLass{
+    tNode root;
+
+    public BST() {
+        root = null;
+    }
+
+    //Insert method
+    //Standard BST insertion
+
+    public tNode insert(tNode node, int key) {
+        if (node == null) {
+            return new tNode(key);
+        }
+        if (key < node.key) {
+            node.left = insert(node.left, key);
+        } else if (key > node.key) {
+            node.right = insert(node.right, key);
+        }
+        return node;
+    }
+    //Build perfect bst
+    //Uses devide and conquer
 }
